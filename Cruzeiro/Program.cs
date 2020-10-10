0701937736e3b91944c9ac34d56cc2a90bc4dd6a
Operaho <emanuelllevi7770@gmail.com>
2020-10-10 20:33:17 -0300

    Update Program.cs

--- Cruzeiro/Program.cs
+++ Cruzeiro/Program.cs
@@ -17,7 +17,7 @@ namespace MyFirstBot
         {
             discord = new DiscordClient(new DiscordConfiguration
             {
-                Token = "NzYzODk1MDg3MTkyNzM1NzQ0.X3-WvQ.zWV014RQ6VI40eOL5RDSZ4XH_wI",
+                Token = "youknowicantsaymytokeninpublic",
                 TokenType = TokenType.Bot
             });
             await discord.ConnectAsync();
